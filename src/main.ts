ef5a70d8ecfea5f6e813c6ff1efe3d682ac291ad
sonders <778277514@qq.com>
2022-06-06 09:43:06 +0800

    提交代码

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
 // import Antd from 'ant-design-vue';
 // import 'ant-design-vue/dist/antd.css';
-const app = createApp(App)
-app.use(router)
-app.use(antd) // 打包时使用
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(antd); // 打包时使用
+app.mount("#app");
